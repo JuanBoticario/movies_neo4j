1587314d6118b108cdf632399686a99a04368376
JuanBoticario <juanboticario@gmail.com>
2024-10-31 18:57:30 +0100

    clear

--- clear.py
+++ clear.py
@@ -1,5 +1,6 @@
 import click
-from queries import DELETE_DB_QUERY
+from queries import DELETE_NAMES_QUERY
+from queries import DELETE_TITLES_QUERY
 from utils import execute_query
 
 @click.command()
@@ -8,5 +9,7 @@ def clear():
     clears all data from the database.
     usage: python3 main.py clear
     """
-    execute_query(DELETE_DB_QUERY)
-    click.echo("All data has been cleared from database.")
+    execute_query(DELETE_NAMES_QUERY)
+    click.echo("All Names have been cleared from database.")
+    execute_query(DELETE_TITLES_QUERY)
+    click.echo("All Titles have been cleared from database.")
